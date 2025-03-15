cac30a6ed03f8714c47fafa2119f0130f0e4fc02
darkeny <darkenpaulo00@gmail.com>
2025-03-15 17:14:09 +0200

    build project

---
+++ ui/src/page/Home/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <img 
+        src="/Meeting.png" 
+        alt="Meeting" 
+        className="max-w-full max-h-full object-contain"
+      />
+    </div>
+  );
+}
